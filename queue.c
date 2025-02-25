c800d6d6e24370f9657421f689e34df10a1c5fa2
Shih-Hsin Huang <tony268596@gmail.com>
2025-02-25 20:46:10 +0800

    Implement queue function
    Using the Linux Kernel API, we can implement the queue functions in a
    clean code style.
    
    The following functions have been tested.
    - q_size()
    - q_new()
    
    Change-Id: I54bf588d5a15c81b7d2003380b9259f7b2fee6e2

--- queue.c
+++ queue.c
@@ -7,7 +7,12 @@
 /* Create an empty queue */
 struct list_head *q_new()
 {
-    return NULL;
+    struct list_head *head = malloc(sizeof(struct list_head));
+    if (!head)
+        return NULL;
+
+    INIT_LIST_HEAD(head);
+    return head;
 }
 
 /* Free all storage used by queue */
@@ -40,7 +45,15 @@ element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
 /* Return number of elements in queue */
 int q_size(struct list_head *head)
 {
-    return -1;
+    if (!head)
+        return 0;
+
+    int len = 0;
+    struct list_head *li;
+
+    list_for_each (li, head)
+        len++;
+    return len;
 }
 
 /* Delete the middle node in queue */
