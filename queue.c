ff6f01c3e5ab02001e7be1ff4d6bf0e571828f20
Shih-Hsin Huang <tony268596@gmail.com>
2025-02-26 10:54:43 +0800

    Implement queue remove functions
    Add q_remove_head and q_remove_tail functions to handle element removal
    from the queue. Using from the Linux Kernel API.
    
    Change-Id: I0ffd7685dd94f1992528c8ba0b7239ace77e7dfa

--- queue.c
+++ queue.c
@@ -47,13 +47,27 @@ bool q_insert_tail(struct list_head *head, char *s)
 /* Remove an element from head of queue */
 element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    if (!sp || !head || !head->next)
+        return NULL;
+    element_t *tmp = container_of(head->next, element_t, list);
+    list_del_init(head->next);
+
+    if (tmp->value)
+        strncpy(sp, tmp->value, bufsize);
+    return tmp;
 }
 
 /* Remove an element from tail of queue */
 element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    if (!sp || !head || !head->next)
+        return NULL;
+    element_t *tmp = container_of(head->prev, element_t, list);
+    list_del_init(head->prev);
+
+    if (tmp->value)
+        strncpy(sp, tmp->value, bufsize);
+    return tmp;
 }
 
 /* Return number of elements in queue */
