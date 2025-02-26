f2f59763dc791b766d65304147f2c44c205db3f5
Shih-Hsin Huang <tony268596@gmail.com>
2025-02-26 14:08:15 +0800

    Implement q_free
    Implement q_free to free all storage used by the queue, including the
    head.
    
    Change-Id: I731397795b27cf7bd8bdbda2123288576343d9cd

--- queue.c
+++ queue.c
@@ -16,7 +16,20 @@ struct list_head *q_new()
 }
 
 /* Free all storage used by queue */
-void q_free(struct list_head *head) {}
+void q_free(struct list_head *head)
+{
+    if (!head)
+        return;
+
+    element_t *entry = NULL;
+    element_t *safe = NULL;
+    list_for_each_entry_safe (entry, safe, head, list) {
+        list_del(&entry->list);
+        q_release_element(entry);
+    }
+    free(head);
+    return;
+}
 
 /* Insert an element at head of queue */
 bool q_insert_head(struct list_head *head, char *s)
